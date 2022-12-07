e4a3ca88a9ea38322d6654822bcccc83b1277b73
hylianwhovian <70862759+hylianwhovian@users.noreply.github.com>
2022-12-07 18:50:32 +0000

    big progress :)

--- Nutrition_Dashboard_Sample_Data.py
+++ Nutrition_Dashboard_Sample_Data.py
@@ -5,7 +5,7 @@ import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.COSMO])
 
-df = pd.read_csv("sample_data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/eleanorb96/cse314_group-project/bhoshaw/updated_mcd_data.csv")
 
 # 'McDonalds', 'Subway', 'Taco Bell', 'Chick-Fil-A', 'Wendy\'s', 'Burger King', 'Domino\'s', 'Panera Bread', 'Pizza Hut', 'Chipotle'
 
@@ -31,18 +31,30 @@ app.layout = html.Div(children = [
                 placeholder="Select Food Items"
             ),
             dcc.RadioItems(
-                ["nf_calories", "nf_total_fat"],
-                "nf_calories",
+                ["Calories", "Nutrients"],
+                "Calories",
                 id = "feature"
             ),
             dcc.Graph(id="graph-of-calories"),
             html.Br(),
-            html.Table([
-                html.Tr([html.Td("Total Calories:"), html.Td(id='calorie-amount')]),
-                html.Tr([html.Td("Total Fats:"), html.Td(id='fat-amount')]),
-                html.Tr([html.Td("Total Cholesterol:"), html.Td(id='chol-amount')]),
+            dbc.Table([
+                html.Thead(html.Tr([html.Th("Nutrition Fact"), html.Th("Amount"), html.Th("Daily Value")])),
+                html.Tr([html.Td("Total Calories:"), html.Td(id='calorie-amount'), html.Td(id="daily-calories")]),
+                html.Tr([html.Td("Total Fat:"), html.Td(id='fat-amount'), html.Td(id="daily-fats")]),
+                html.Tr([html.Td("Saturated Fat:"), html.Td(id='satfat-amount'), html.Td(id="daily-satfat")]),
+                html.Tr([html.Td("Trans Fat:"), html.Td(id='tranfat-amount'), html.Td("")]),
+                html.Tr([html.Td("Cholesterol:"), html.Td(id='chol-amount'), html.Td(id="daily-chol")]),
+                html.Tr([html.Td("Sodium:"), html.Td(id='sodium-amount'), html.Td(id="daily-sodium")]),
+                html.Tr([html.Td("Total Carbohydrate:"), html.Td(id='carb-amount'), html.Td(id="daily-carb")]),
+                html.Tr([html.Td("Dietary Fiber:"), html.Td(id='fiber-amount'), html.Td(id="daily-fiber")]),
+                html.Tr([html.Td("Sugars:"), html.Td(id='sugar-amount'), html.Td("")]),
+                html.Tr([html.Td("Protein:"), html.Td(id='protein-amount'), html.Td(id="daily-protein")]),
+                html.Tr([html.Td("Vitamin A:"), html.Td(""), html.Td(id="daily-A")]),
+                html.Tr([html.Td("Vitamin C:"), html.Td(""), html.Td(id="daily-C")]),
+                html.Tr([html.Td("Calcium:"), html.Td(""), html.Td(id="daily-calcium")]),
+                html.Tr([html.Td("Iron:"), html.Td(""), html.Td(id="daily-iron")]),
             ]),
-        ], style={'padding': 10, 'flex': 1}), 
+        ], style={'padding': 10, 'flex': 1}),
         html.Div([
             dcc.Dropdown(
                 df['brand_name'].unique(),
@@ -56,16 +68,28 @@ app.layout = html.Div(children = [
                 placeholder="Select Food Items",
             ),
             dcc.RadioItems(
-                ["nf_calories", "nf_total_fat"],
-                "nf_calories",
+                ["Calories", "Nutrients"],
+                "Calories",
                 id = "feature2"
             ),
             dcc.Graph(id="graph-of-calories2"),
             html.Br(),
-            html.Table([
-                html.Tr([html.Td("Total Calories:"), html.Td(id='calorie-amount2')]),
-                html.Tr([html.Td("Total Fats:"), html.Td(id='fat-amount2')]),
-                html.Tr([html.Td("Total Cholesterol:"), html.Td(id='chol-amount2')]),
+            dbc.Table([
+                html.Thead(html.Tr([html.Th("Nutrition Fact"), html.Th("Amount"), html.Th("Daily Value")])),
+                html.Tr([html.Td("Total Calories:"), html.Td(id='calorie-amount2'), html.Td(id="daily-calories2")]),
+                html.Tr([html.Td("Total Fat:"), html.Td(id='fat-amount2'), html.Td(id="daily-fats2")]),
+                html.Tr([html.Td("Saturated Fat:"), html.Td(id='satfat-amount2'), html.Td(id="daily-satfat2")]),
+                html.Tr([html.Td("Trans Fat:"), html.Td(id='tranfat-amount2'), html.Td("")]),
+                html.Tr([html.Td("Cholesterol:"), html.Td(id='chol-amount2'), html.Td(id="daily-chol2")]),
+                html.Tr([html.Td("Sodium:"), html.Td(id='sodium-amount2'), html.Td(id="daily-sodium2")]),
+                html.Tr([html.Td("Total Carbohydrate:"), html.Td(id='carb-amount2'), html.Td(id="daily-carb2")]),
+                html.Tr([html.Td("Dietary Fiber:"), html.Td(id='fiber-amount2'), html.Td(id="daily-fiber2")]),
+                html.Tr([html.Td("Sugars:"), html.Td(id='sugar-amount2'), html.Td("")]),
+                html.Tr([html.Td("Protein:"), html.Td(id='protein-amount2'), html.Td(id="daily-protein2")]),
+                html.Tr([html.Td("Vitamin A:"), html.Td(""), html.Td(id="daily-A2")]),
+                html.Tr([html.Td("Vitamin C:"), html.Td(""), html.Td(id="daily-C2")]),
+                html.Tr([html.Td("Calcium:"), html.Td(""), html.Td(id="daily-calcium2")]),
+                html.Tr([html.Td("Iron:"), html.Td(""), html.Td(id="daily-iron2")]),
             ]),
         ], style={'padding': 10, 'flex': 1}),
     
@@ -95,6 +119,8 @@ def update_comparison(brand1, items1, brand2, items2):
     filtered_df1 = filtered_df_item1[filtered_df_item1['item_name'].isin(items1)]
     calorie_total1 = sum(filtered_df1["nf_calories"])
     fat_total1 = sum(filtered_df1["nf_total_fat"])
+    carb_total1 = sum(filtered_df1["nf_total_carbohydrate"])
+    protein_total1 = sum(filtered_df1["nf_protein"])
 
     filtered_df_item2 = df[df["brand_name"] == brand2]
     if items2 is None:
@@ -102,36 +128,37 @@ def update_comparison(brand1, items1, brand2, items2):
     filtered_df2 = filtered_df_item2[filtered_df_item2['item_name'].isin(items2)]
     calorie_total2 = sum(filtered_df2["nf_calories"])
     fat_total2 = sum(filtered_df2["nf_total_fat"])
+    carb_total2 = sum(filtered_df2["nf_total_carbohydrate"])
+    protein_total2 = sum(filtered_df2["nf_protein"])
 
-    if fat_total2==0:
-        fat_total2+=1
-    if calorie_total2==0:
-        calorie_total2+=1
-    if fat_total1==0:
-        fat_total1+=1
-    
-    if calorie_total1==0:
-        calorie_total1+=1
+    calfat1 = fat_total1*9
+    calfat2 = fat_total2*9
+    calcarb1 = carb_total1*4
+    calcarb2 = carb_total2*4
+    calpro1 = protein_total1*4
+    calpro2 = protein_total2*4
 
-    calrat = calorie_total1/calorie_total2
-    fatrat = fat_total1/fat_total2
+    fatdif = calfat1-calfat2
+    carbdif = calcarb1 - calcarb2
+    prodif = calpro1 - calpro2
+    caldif = calorie_total1 - calorie_total2
 
-    if calrat < 1:
+    calcol = "red"
+    fatcol = "red"
+    carbcol = "red"
+    procol = "red"
+
+    if caldif < 0:
         calcol = "green"
-    else:
-        calcol = "red"
-
-    if fatrat < 1:
+    if fatdif < 0:
         fatcol = "green"
-    else:
-        fatcol = "red"
+    if carbdif < 0:
+        carbcol = "green"
+    if prodif < 0:
+        procol = "green"
     
-
-    fig = px.bar(x = ["Calories", "Fat"], y = [calrat, fatrat], title="Nutrion Ratios (Meal 1 / Meal 2)", labels={"x":"Nutrition Statistic", "y":"Ratio"})
-    fig.update_traces(marker_color=[calcol, fatcol])
-    fig.add_hline(y=1)
-
-
+    fig = px.bar(x = ["Calorie Difference", "Calories from Fat Difference", "Calories from Carbs Difference", "Calories from Protein Difference"], y = [caldif, fatdif, carbdif, prodif], title="Nutrion Differences (Meal 1 - Meal 2)", labels={"x":"Nutrition Statistic", "y":"Difference"})
+    fig.update_traces(marker_color=[calcol, fatcol, carbcol, procol])
 
     return fig
 
@@ -164,18 +191,57 @@ def update_graph(input_brand, input_feature, food_choices):
     filtered_df_item = df[df["brand_name"] == input_brand]
     if food_choices is None:
         food_choices = list(filtered_df_item['item_name'][0])
-
+    
     filtered_df = filtered_df_item[filtered_df_item['item_name'].isin(food_choices)]
 
-    fig = px.bar(filtered_df, x = "item_name", y=input_feature)
-    fig.update_layout(transition_duration = 400)
+    if input_feature == "Calories":
+        fig = px.bar(filtered_df, x = "item_name", y="nf_calories", labels={
+            "nf_calories" : "Calories",
+            "item_name" : "Item"
+        })
 
+    elif input_feature == "Nutrients":
+        fig = px.bar(filtered_df, x = "item_name", y = ["nf_total_fat", "nf_total_carbohydrate", "nf_protein"], barmode="group", labels={
+            "item_name" : "Item",
+            "value" : "Grams",
+            "variable" : "Nutrient",
+            "nf_total_fat" : "Fat"
+        })
+        newnames = {'nf_total_fat':'Fat', 'nf_total_carbohydrate': 'Carbs', "nf_protein" : "Protein"}
+        fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
+                                      legendgroup = newnames[t.name],
+                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
+                                     )
+                  )
+
+    
+    fig.update_layout(transition_duration = 400)
     return fig
+
 
 @app.callback(
     Output(component_id='calorie-amount', component_property='children'),
+    Output(component_id='daily-calories', component_property='children'),
     Output(component_id='fat-amount', component_property='children'),
+    Output(component_id='daily-fats', component_property='children'),
+    Output(component_id='satfat-amount', component_property='children'),
+    Output(component_id='daily-satfat', component_property='children'),
+    Output(component_id='tranfat-amount', component_property='children'),
     Output(component_id='chol-amount', component_property='children'),
+    Output(component_id='daily-chol', component_property='children'),
+    Output(component_id='sodium-amount', component_property='children'),
+    Output(component_id='daily-sodium', component_property='children'),
+    Output(component_id='carb-amount', component_property='children'),
+    Output(component_id='daily-carb', component_property='children'),
+    Output(component_id='fiber-amount', component_property='children'),
+    Output(component_id='daily-fiber', component_property='children'),
+    Output(component_id='sugar-amount', component_property='children'),
+    Output(component_id='protein-amount', component_property='children'),
+    Output(component_id='daily-protein', component_property='children'),
+    Output(component_id='daily-A', component_property='children'),
+    Output(component_id='daily-C', component_property='children'),
+    Output(component_id='daily-calcium', component_property='children'),
+    Output(component_id='daily-iron', component_property='children'),
     Input(component_id='my-brand', component_property='value'),
     Input(component_id='food-options', component_property='value')
 )
@@ -184,11 +250,31 @@ def word_output(input_brand, food_choices):
     if food_choices is None:
         food_choices = list(filtered_df_item['item_name'][0])
     filtered_df = filtered_df_item[filtered_df_item['item_name'].isin(food_choices)]
-    calorie_total = sum(filtered_df["nf_calories"])
-    fat_total = sum(filtered_df["nf_total_fat"])
-    chol_total = sum(filtered_df["nf_cholesterol"])
+    calorie_total = str(sum(filtered_df["nf_calories"]))+"g"
+    calorie_dv = str(round((sum(filtered_df["nf_calories"])/2000.0)*100))+"%"
+    fat_total = str(sum(filtered_df["nf_total_fat"]))+"g"
+    fat_dv = str(round((sum(filtered_df["nf_total_fat"])/78.0)*100))+"%"
+    satfat_total = str(sum(filtered_df["nf_saturated_fat"]))+"g"
+    satfat_dv = str(round((sum(filtered_df["nf_saturated_fat"])/20.0)*100))+"%"
+    tranfat_total = str(sum(filtered_df["nf_trans_fatty_acid"]))+"g"
+    chol_total = str(sum(filtered_df["nf_cholesterol"]))+"mg"
+    chol_dv = str(round((sum(filtered_df["nf_cholesterol"])/300.0)*100))+"%"
+    sodium_total = str(sum(filtered_df["nf_sodium"]))+"mg"
+    sodium_dv = str(round((sum(filtered_df["nf_sodium"])/2300.0)*100))+"%"
+    carb_total = str(sum(filtered_df["nf_total_carbohydrate"]))+"g"
+    carb_dv = str(round((sum(filtered_df["nf_total_carbohydrate"])/275.0)*100))+"%"
+    fiber_total = str(sum(filtered_df["nf_dietary_fiber"]))+"g"
+    fiber_dv = str(round((sum(filtered_df["nf_dietary_fiber"])/28.0)*100))+"%"
+    sugar_total = str(sum(filtered_df["nf_sugars"]))+"g"
+    protein_total = str(sum(filtered_df["nf_protein"]))+"g"
+    protein_dv = str(round((sum(filtered_df["nf_protein"])/50.0)*100))+"%"
 
-    return calorie_total, fat_total, chol_total
+    vitamin_a_dv = str(sum(filtered_df["nf_vitamin_a_dv"]))+"%"
+    vitamin_c_dv = str(sum(filtered_df["nf_vitamin_c_dv"]))+"%"
+    calcium_dv = str(sum(filtered_df["nf_calcium_dv"]))+"%"
+    iron_dv = str(sum(filtered_df["nf_iron_dv"]))+"%"
+
+    return calorie_total, calorie_dv, fat_total, fat_dv, satfat_total, satfat_dv, tranfat_total, chol_total, chol_dv, sodium_total, sodium_dv, carb_total, carb_dv, fiber_total, fiber_dv, sugar_total, protein_total, protein_dv, vitamin_a_dv, vitamin_c_dv, calcium_dv, iron_dv
 
 ## LEFT SIDE
 
@@ -215,18 +301,52 @@ def update_graph(input_brand, input_feature, food_choices):
     filtered_df_item = df[df["brand_name"] == input_brand]
     if food_choices is None:
         food_choices = list(filtered_df_item['item_name'][0])
-
-    filtered_df = filtered_df_item[filtered_df_item['item_name'].isin(food_choices)]
     
-    fig = px.bar(filtered_df, x = "item_name", y=input_feature)
-    fig.update_layout(transition_duration = 400)
+    filtered_df = filtered_df_item[filtered_df_item['item_name'].isin(food_choices)]
 
+    if input_feature == "Calories":
+        fig = px.bar(filtered_df, x = "item_name", y="nf_calories")
+
+    elif input_feature == "Nutrients":
+        fig = px.bar(filtered_df, x = "item_name", y = ["nf_total_fat", "nf_total_carbohydrate", "nf_protein"], barmode="group", labels={
+            "item_name" : "Item",
+            "value" : "Grams",
+            "variable" : "Nutrient",
+            "nf_total_fat" : "Fat"
+        })
+        newnames = {'nf_total_fat':'Fat', 'nf_total_carbohydrate': 'Carbs', "nf_protein" : "Protein"}
+        fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
+                                      legendgroup = newnames[t.name],
+                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
+                                     )
+                  )
+    
+    fig.update_layout(transition_duration = 400)
     return fig
 
 @app.callback(
     Output(component_id='calorie-amount2', component_property='children'),
+    Output(component_id='daily-calories2', component_property='children'),
     Output(component_id='fat-amount2', component_property='children'),
+    Output(component_id='daily-fats2', component_property='children'),
+    Output(component_id='satfat-amount2', component_property='children'),
+    Output(component_id='daily-satfat2', component_property='children'),
+    Output(component_id='tranfat-amount2', component_property='children'),
     Output(component_id='chol-amount2', component_property='children'),
+    Output(component_id='daily-chol2', component_property='children'),
+    Output(component_id='sodium-amount2', component_property='children'),
+    Output(component_id='daily-sodium2', component_property='children'),
+    Output(component_id='carb-amount2', component_property='children'),
+    Output(component_id='daily-carb2', component_property='children'),
+    Output(component_id='fiber-amount2', component_property='children'),
+    Output(component_id='daily-fiber2', component_property='children'),
+    Output(component_id='sugar-amount2', component_property='children'),
+    Output(component_id='protein-amount2', component_property='children'),
+    Output(component_id='daily-protein2', component_property='children'),
+    Output(component_id='daily-A2', component_property='children'),
+    Output(component_id='daily-C2', component_property='children'),
+    Output(component_id='daily-calcium2', component_property='children'),
+    Output(component_id='daily-iron2', component_property='children'),
     Input(component_id='my-brand2', component_property='value'),
     Input(component_id='food-options2', component_property='value')
 )
@@ -235,11 +355,31 @@ def word_output(input_brand, food_choices):
     if food_choices is None:
         food_choices = list(filtered_df_item['item_name'][0])
     filtered_df = filtered_df_item[filtered_df_item['item_name'].isin(food_choices)]
-    calorie_total = sum(filtered_df["nf_calories"])
-    fat_total = sum(filtered_df["nf_total_fat"])
-    chol_total = sum(filtered_df["nf_cholesterol"])
+    calorie_total = str(sum(filtered_df["nf_calories"]))+"g"
+    calorie_dv = str(round((sum(filtered_df["nf_calories"])/2000.0)*100))+"%"
+    fat_total = str(sum(filtered_df["nf_total_fat"]))+"g"
+    fat_dv = str(round((sum(filtered_df["nf_total_fat"])/78.0)*100))+"%"
+    satfat_total = str(sum(filtered_df["nf_saturated_fat"]))+"g"
+    satfat_dv = str(round((sum(filtered_df["nf_saturated_fat"])/20.0)*100))+"%"
+    tranfat_total = str(sum(filtered_df["nf_trans_fatty_acid"]))+"g"
+    chol_total = str(sum(filtered_df["nf_cholesterol"]))+"mg"
+    chol_dv = str(round((sum(filtered_df["nf_cholesterol"])/300.0)*100))+"%"
+    sodium_total = str(sum(filtered_df["nf_sodium"]))+"mg"
+    sodium_dv = str(round((sum(filtered_df["nf_sodium"])/2300.0)*100))+"%"
+    carb_total = str(sum(filtered_df["nf_total_carbohydrate"]))+"g"
+    carb_dv = str(round((sum(filtered_df["nf_total_carbohydrate"])/275.0)*100))+"%"
+    fiber_total = str(sum(filtered_df["nf_dietary_fiber"]))+"g"
+    fiber_dv = str(round((sum(filtered_df["nf_dietary_fiber"])/28.0)*100))+"%"
+    sugar_total = str(sum(filtered_df["nf_sugars"]))+"g"
+    protein_total = str(sum(filtered_df["nf_protein"]))+"g"
+    protein_dv = str(round((sum(filtered_df["nf_protein"])/50.0)*100))+"%"
 
-    return calorie_total, fat_total, chol_total
+    vitamin_a_dv = str(sum(filtered_df["nf_vitamin_a_dv"]))+"%"
+    vitamin_c_dv = str(sum(filtered_df["nf_vitamin_c_dv"]))+"%"
+    calcium_dv = str(sum(filtered_df["nf_calcium_dv"]))+"%"
+    iron_dv = str(sum(filtered_df["nf_iron_dv"]))+"%"
+
+    return calorie_total, calorie_dv, fat_total, fat_dv, satfat_total, satfat_dv, tranfat_total, chol_total, chol_dv, sodium_total, sodium_dv, carb_total, carb_dv, fiber_total, fiber_dv, sugar_total, protein_total, protein_dv, vitamin_a_dv, vitamin_c_dv, calcium_dv, iron_dv
 
 
 if __name__ == '__main__':
